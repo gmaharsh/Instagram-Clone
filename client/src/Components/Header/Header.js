e0284bb629c25b70e0811b2d09f3a042e4d1797f
Maharsh <gmaharsh@yahoo.com>
2020-12-28 15:03:32 -0600

    Added Header

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './Header.css';
+import HomeIcon from '@material-ui/icons/Home';
+import MessageIcon from '@material-ui/icons/Message';
+import ExploreIcon from '@material-ui/icons/Explore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Avatar from "@material-ui/core/Avatar";
 
 function Header() {
     return (
         <div className="header">
-            <h1>I am on Header router</h1>
+            <div className="header__logo">
+                <h3>Instagram</h3>
+            </div>
+            <div className="header__search">
+                <input
+                    type="text"
+                    placeholder="Search"
+                />
+            </div>
+            <div className="header__items">
+                <div className="header__item">
+                    <HomeIcon />
+                </div>
+                <div className="header__item">
+                    <MessageIcon />
+                </div>
+                <div className="header__item">
+                    <ExploreIcon />
+                </div>
+                <div className="header__item">
+                    <FavoriteIcon />
+                </div>
+                <div className="header__item">
+                    <Avatar />
+                </div>
+            </div>
         </div>
     )
 }
