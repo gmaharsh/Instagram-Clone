69b164ca35bed1be6ff0bebec2f447763702d88f
Maharsh <gmaharsh@yahoo.com>
2020-12-27 21:37:57 -0600

    Connected to Database

--- server/app.js
+++ server/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const mongoose = require('mongoose');
+const {MONGO_URI} = require('./keys')
+
 //Middlwares
 
 
-//Routes
-app.get("/", (req, res) => {
-    res.send("Hello World")
+//Database Connection
+const connection_url = `mongodb+srv://gmaharsh:Admin@456123@one-stop.n0brx.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+mongoose.connect(connection_url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 })
+    .then(() => {
+        console.log("Connected to Database")
+    })
+    .catch((error) => {
+        console.log("Error:-",error)
+    })
+
+//Routes
+
 
 //Listener
 app.listen(PORT, () => {
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI : "mongodb+srv://gmaharsh:<password>@cluster0.9gzq7.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
