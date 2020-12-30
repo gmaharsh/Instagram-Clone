bb647c33d4c0dc80d6aa7c08eaeb5fde4c1fd367
Maharsh <gmaharsh@yahoo.com>
2020-12-29 23:13:52 -0600

    Added fetching post from database option

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 import Avatar from '@material-ui/core/Avatar';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
@@ -11,121 +11,86 @@ import { useStateValue } from '../../reducers/StateProvider';
 function Home() {
 
     const [state, dispatch] = useStateValue();
+    const [data, setData] = useState([]);
 
     console.log("State From Home:-", state)
-        
+
+    useEffect(() => {
+        fetch('/allpost', {
+            header: {
+                "Authorization" : "Bearer " + localStorage.getItem("jwt")
+            }
+        }).then(res => res.json())
+            .then(result => {
+                setData(result.post)
+        })
+    }, [])
+    
+    console.log(data)
 
     return (
         <div className="home">
-            <div className="home__post">
-                <div className="home__displayInfo">
-                    <div className="display__info">
-                        <Avatar /> 
-                        <h4>maharsh.gheewala</h4>
-                    </div>
-                    <div className="displayInfo__more">
-                        <MoreHorizIcon />
-                    </div>
-                </div>
-                <div className="home__postImage">
-                    <img
-                        src="https://images.unsplash.com/photo-1609024849543-ff59df361d08?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt=""
-                    />
-                </div>
-                <div className="home__postInfo">
-                    <div className="post__Analytics">
-                        <div className="post__AnalyticsLeft">
-                            <div className="post__AnalyticsLeftItem">
-                                <FavoriteBorderIcon fontSize="medium" />
+            {data.map(item => {
+                    return (
+                        <div className="home__post">
+                            <div className="home__displayInfo">
+                                <div className="display__info">
+                                    <Avatar /> 
+                                    <h4>{item.postedBy.name}</h4>
+                                </div>
+                                <div className="displayInfo__more">
+                                    <MoreHorizIcon />
+                                </div>
                             </div>
-                            <div className="post__AnalyticsLeftItem">
-                                <ChatRoundedIcon fontSize="medium" />
+                            <div className="home__postImage">
+                                <img
+                                    src={item.image} alt=""
+                                />
                             </div>
-                            <div className="post__AnalyticsLeftItem">
-                                <SendIcon fontSize="medium" />
+                            <div className="home__postInfo">
+                                <div className="post__Analytics">
+                                    <div className="post__AnalyticsLeft">
+                                        <div className="post__AnalyticsLeftItem">
+                                            <FavoriteBorderIcon fontSize="medium" />
+                                        </div>
+                                        <div className="post__AnalyticsLeftItem">
+                                            <FavoriteBorderIcon fontSize="medium" />
+                                        </div>
+                                        <div className="post__AnalyticsLeftItem">
+                                            <ChatRoundedIcon fontSize="medium" />
+                                        </div>
+                                        <div className="post__AnalyticsLeftItem">
+                                            <SendIcon fontSize="medium" />
+                                        </div>
+                                    </div>
+                                    <div className="post__AnalyticsRight">
+                                        <BookmarkBorderRoundedIcon />
+                                    </div>  
+                                </div>
+                                <div className="post__LikesComment">
+                                    <h4>3,000 likes</h4>
+                                    <div className="post__Comment">
+                                        <h4>{item.postedBy.name}</h4>
+                                        <p>{item.caption}</p>
+                                    </div>
+                                    <p>1 day ago</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="post__AnalyticsRight">
-                            <BookmarkBorderRoundedIcon />
-                        </div>  
-                    </div>
-                    <div className="post__LikesComment">
-                        <h4>3,000 likes</h4>
-                        <div className="post__Comment">
-                            <h4>maharsh.gheewala</h4>
-                            <p>hello</p>
-                        </div>
-                        <p>1 day ago</p>
-                    </div>
-                </div>
-                <div className="home__addComment">
-                    <div className="newComment">
-                        <input
-                            type="text"
-                            placeholder="Add a comment"
-                        />
-                    </div>
-                    <div className="postButton">
-                        <h4>Post</h4>
-                    </div>
-                </div>
-            </div>
-            <div className="home__post">
-                <div className="home__displayInfo">
-                    <div className="display__info">
-                        <Avatar /> 
-                        <h4>maharsh.gheewala</h4>
-                    </div>
-                    <div className="displayInfo__more">
-                        <MoreHorizIcon />
-                    </div>
-                </div>
-                <div className="home__postImage">
-                    <img
-                        src="https://images.unsplash.com/photo-1609024849543-ff59df361d08?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt=""
-                    />
-                </div>
-                <div className="home__postInfo">
-                    <div className="post__Analytics">
-                        <div className="post__AnalyticsLeft">
-                            <div className="post__AnalyticsLeftItem">
-                                <FavoriteBorderIcon fontSize="medium" />
-                            </div>
-                            <div className="post__AnalyticsLeftItem">
-                                <FavoriteBorderIcon fontSize="medium" />
-                            </div>
-                            <div className="post__AnalyticsLeftItem">
-                                <ChatRoundedIcon fontSize="medium" />
-                            </div>
-                            <div className="post__AnalyticsLeftItem">
-                                <SendIcon fontSize="medium" />
+                            <div className="home__addComment">
+                                <div className="newComment">
+                                    <input
+                                        type="text"
+                                        placeholder="Add a comment"
+                                    />
+                                </div>
+                                <div className="postButton">
+                                    <h4>Post</h4>
+                                </div>
                             </div>
                         </div>
-                        <div className="post__AnalyticsRight">
-                            <BookmarkBorderRoundedIcon />
-                        </div>  
-                    </div>
-                    <div className="post__LikesComment">
-                        <h4>3,000 likes</h4>
-                        <div className="post__Comment">
-                            <h4>maharsh.gheewala</h4>
-                            <p>hello</p>
-                        </div>
-                        <p>1 day ago</p>
-                    </div>
-                </div>
-                <div className="home__addComment">
-                    <div className="newComment">
-                        <input
-                            type="text"
-                            placeholder="Add a comment"
-                        />
-                    </div>
-                    <div className="postButton">
-                        <h4>Post</h4>
-                    </div>
-                </div>
-            </div>
+                    )
+                })
+            }
         </div>
     )
 }
